15dfa77ef8954a45e50dd1d63ddd540a505d6886
kraenkthetank <codingTwitter@web.de>
2024-07-11 12:56:06 +0200

    added paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TicketOverviewComponent } from './ticket-overview/ticket-overview.component';
 import { TicketViewComponent } from './ticket-view/ticket-view.component';
+import { TicketUnassingedComponent } from './ticket-unassinged/ticket-unassinged.component';
+import { TicketCreateComponent } from './ticket-create/ticket-create.component';
 
 export const routes: Routes = [
     {
@@ -11,7 +13,9 @@ export const routes: Routes = [
         children: [
             {path: 'dashboard', component: DashboardComponent},
             {path: 'ticket-overview', component: TicketOverviewComponent},
+            {path: 'ticket-unassigned', component: TicketUnassingedComponent},
             {path: 'ticket/:id', component: TicketViewComponent},
+            {path: 'ticket-create', component: TicketCreateComponent},
             {path: '',redirectTo:'/dashboard', pathMatch: 'full'}
 
         ],
