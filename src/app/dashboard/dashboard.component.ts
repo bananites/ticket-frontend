a5c244bd72494fc7f1c92e48c5be5dbb4d9cbbad
kraenkthetank <codingTwitter@web.de>
2024-07-11 12:52:57 +0200

    added new icon

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,11 +3,12 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, MatIconModule, MatGridListModule],
+  imports: [RouterModule, MatListModule, MatButtonModule, MatIconModule, MatGridListModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
