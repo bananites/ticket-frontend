0895252920b4781051342e45b5d993e114e32fad
kraenkthetank <codingTwitter@web.de>
2024-07-11 12:55:44 +0200

    changed name and added attribute

--- src/app/ticket-view/ticket-view.component.ts
+++ src/app/ticket-view/ticket-view.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Tickets } from '../models/ticket.interface';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Ticket } from '../models/ticket.interface';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import { MatDividerModule } from "@angular/material/divider"; 
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
 
 @Component({
@@ -17,14 +17,18 @@ import { MatIconModule } from "@angular/material/icon";
 export class TicketViewComponent implements OnInit {
 
 
-  tickets: Tickets[] = [
+  tickets: Ticket[] = [
     {
       id: '1',
       title: 'Photos',
       description: 'lorem ipsum ksaojfw  aiwfoai oinfoifn aonacnoiaooi oij aop cjooi oai o c',
       updated: new Date('2024-05-26').toLocaleDateString(),
       created: new Date('12/31/15').toLocaleDateString(), // Beispiel für das Erstellungsdatum
-      status: 'completed' // Beispiel für den Status
+      owner: 'Pascal Breuer',
+      status: 'completed', // Beispiel für den Status
+
+     
+
     },
     {
       id: '2',
@@ -32,7 +36,10 @@ export class TicketViewComponent implements OnInit {
       description: 'lorem ipsum ksaojfw  aiwfoai oinfoifn aonacnoiaooi oij aop cjooi oai o c',
       updated: new Date('1/17/16').toLocaleDateString(),
       created: new Date('1/10/16').toLocaleDateString(), // Beispiel für das Erstellungsdatum
-      status: 'in-progress' // Beispiel für den Status
+      owner: 'Pascal Breuer',
+      status: 'in-progress',// Beispiel für den Status
+
+
     },
     {
       id: '3',
@@ -40,7 +47,11 @@ export class TicketViewComponent implements OnInit {
       description: 'lorem ipsum ksaojfw  aiwfoai oinfoifn aonacnoiaooi oij aop cjooi oai o c',
       updated: new Date('1/28/16').toLocaleDateString(),
       created: new Date('1/1/16').toLocaleDateString(), // Beispiel für das Erstellungsdatum
-      status: 'pending' // Beispiel für den Status
+      owner: 'Pascal Breuer',
+      status: 'pending' ,// Beispiel für den Status
+   
+
+
     },
   ];
 
@@ -54,10 +65,13 @@ export class TicketViewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.loadTicket(id);
-    }
+    this.route.paramMap.subscribe((params: ParamMap)=> {
+      const id = params.get('id');
+
+      if(id){
+        this.loadTicket(id)
+      }
+    });
   }
 
   loadTicket(id: string): void {
