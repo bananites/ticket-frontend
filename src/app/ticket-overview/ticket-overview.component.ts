d54e8fb85b3af436dc648f630982c1d436a99006
kraenkthetank <codingTwitter@web.de>
2024-07-29 21:41:43 +0200

    api call and function getOwnerName

--- src/app/ticket-overview/ticket-overview.component.ts
+++ src/app/ticket-overview/ticket-overview.component.ts
@@ -1,28 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
-import { DatePipe } from '@angular/common';
+import { DatePipe, JsonPipe } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatListModule } from "@angular/material/list";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { Ticket } from '../models/ticket';
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { log } from 'node:console';
 
 
 @Component({
   selector: 'app-ticket-overview',
   standalone: true,
-  imports: [RouterModule,MatCheckboxModule, MatButtonModule, MatListModule, MatTableModule, MatIconModule, MatDividerModule, DatePipe],
+  imports: [JsonPipe, RouterModule, MatCheckboxModule, MatButtonModule, MatListModule, MatTableModule, MatIconModule, MatDividerModule, DatePipe, HttpClientModule],
   templateUrl: './ticket-overview.component.html',
   styleUrl: './ticket-overview.component.scss'
 })
 export class TicketOverviewComponent {
 
+  displayedColumns: string[] = ['select', 'title', 'owner', 'status', 'updated', 'created'];
 
-displayedColumns: string[] = ['select','title','owner',  'status', 'updated', 'created',];
+  httpClient = inject(HttpClient)
+  public tickets: Array<Ticket> =  []
 
-  // TODO make api call
-  tickets: any 
-   
+  ngOnInit(){
+    this.httpClient.get('http://localhost:3000/api/v1/ticket').subscribe({
+      next: (tickets: any) => {
+        
+        this.tickets = tickets;
+      }, error: (err) => console.log(err)
+    })
+  }
+
+  getOwnerName(user: any): string | undefined{
+     if(!user){
+      return
+     }
+    return user.firstname + " " + user.lastname
+  }
+
+
+
 }
