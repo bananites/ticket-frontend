5e13142e9902856fa029885925bb5c60ef914634
kraenkthetank <codingTwitter@web.de>
2024-08-03 18:27:23 +0200

    added user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,3 +1,28 @@
 export class User{
     
+    private _id: string = "";
+    private _firstname: string = "";
+    private _lastname: string = "";
+   
+   
+   
+   
+    public get id(): string {
+        return this._id;
+    }
+    public set id(value: string) {
+        this._id = value;
+    }
+    public get firstname(): string {
+        return this._firstname;
+    }
+    public set firstname(value: string) {
+        this._firstname = value;
+    }
+    public get lastname(): string {
+        return this._lastname;
+    }
+    public set lastname(value: string) {
+        this._lastname = value;
+    }
 }
