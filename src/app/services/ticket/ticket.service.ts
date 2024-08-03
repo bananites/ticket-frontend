a9eb2d8679a91bb91ebf9f1ce0dbce5ce2cdf0c3
kraenkthetank <codingTwitter@web.de>
2024-08-03 18:28:27 +0200

    added a ticket service, with postTicket for send post to api

---
+++ src/app/services/ticket/ticket.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Ticket } from '../../models/ticket';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from "./../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TicketService {
+
+  
+  constructor(
+    private http: HttpClient
+  ) {}
+   
+
+  postTicket(ticket: any): Observable<Ticket>{
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer your-token',
+      }),
+      params: new HttpParams().set('ownerId', ticket.createdBy).set('title', ticket.title).set('description', ticket.description),
+      responseType: 'json' as const,
+    };
+
+    // console.log(this.http.post<Ticket>(environment.apiUrl + "/ticket", httpOptions))
+    return this.http.post<Ticket>(environment.apiUrl + "/v1/ticket",ticket, httpOptions)
+
+  }
+
+}
