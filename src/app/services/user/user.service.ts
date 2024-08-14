cc30e3cd9ff9e6549bd699fb92f8d53f53b50e0e
kraenkthetank <codingTwitter@web.de>
2024-08-14 21:22:05 +0200

    added userService with getAllUsers()

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  
+
+getAllUser(): Observable<User[]>{ 
+  const httpOptions ={
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+      //TODO think about authorization
+    
+    }),
+    responseType: 'json' as const,
+  };
+
+  return this.http.get<User[]>(environment.apiUrl +"/v1/users", httpOptions)
+}
+  
+}
