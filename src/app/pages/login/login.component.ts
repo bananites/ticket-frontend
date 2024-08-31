bc7b4ad5a4a467cc2db487458e353913c9d26072
bananites <tank@GoMakeGames>
2024-08-31 08:32:33 +0200

    changed the login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
-
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  
 }
