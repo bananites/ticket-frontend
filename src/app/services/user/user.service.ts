3f7f4ced6ca31f89af5184653f0ec2ad300a1afc
bananites <codingTwitter@web.de>
2024-11-11 19:10:17 +0100

    init guard

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -4,17 +4,27 @@ import { User } from '../../models/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
+  getAdminBoard(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      responseType: 'json' as const,
+    }
+    return this.http.get<User>(environment.apiUrl + '/v1/users', httpOptions);
+  }
   getAllUser(): Observable<User[]> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        //TODO think about authorization
+        // TODO think about authorization
         //
       }),
       responseType: 'json' as const,
