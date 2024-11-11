0a1e31839de70bbdffd15af9306356abb7ec8ba5
bananites <codingTwitter@web.de>
2024-11-11 19:13:45 +0100

    init storage service

---
+++ src/app/services/storage/storage.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+const USER_KEY = 'auth-user';
+
+export interface SessionStorage{
+ accessToken: string
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  clean(): void {
+    window.sessionStorage.clear();
+  }
+
+  public saveSession(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+
+  }
+
+  public getSession(): SessionStorage | undefined{
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+
+    return;
+  }
+
+  public isLoggedOn(): boolean {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+}
