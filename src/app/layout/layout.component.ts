c94c49652abb68697e151770df2fa4fa46e16876
bananites <codingTwitter@web.de>
2024-11-11 19:15:20 +0100

    check for logged in

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -19,17 +19,20 @@ import { TicketService } from '../services/ticket/ticket.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TicketOverviewComponent } from '../pages/ticket-overview/ticket-overview.component';
 import { TicketNewListComponent } from '../components/ticket-new-list/ticket-new-list.component';
+import { StorageService } from '../services/storage/storage.service';
+import { AuthUserService } from '../services/auth/auth-user.service';
+import { coerceStringArray } from '@angular/cdk/coercion';
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [TicketOverviewComponent, 
-    CommonModule, 
-    DashboardComponent, 
-    MatTabsModule, 
+  imports: [TicketOverviewComponent,
+    CommonModule,
+    DashboardComponent,
+    MatTabsModule,
     RouterModule,
-    MatInputModule, 
+    MatInputModule,
     MatProgressSpinnerModule,
     FormsModule,
     MatFormFieldModule,
@@ -50,18 +53,31 @@ import { TicketNewListComponent } from '../components/ticket-new-list/ticket-new
 export class LayoutComponent implements OnInit {
 
 
+  isLoggedIn = false;
+  username?: string;
+
   // TODO make api call 
   tickets: Ticket[] = []
   ticketsNew: Ticket[] = []
   isLoading: boolean = true
 
   constructor(
+    private _storageService: StorageService,
+    private _authSerivce: AuthUserService,
     private ticketService: TicketService
-  ) {
+  ) { }
 
-  }
+
 
   ngOnInit(): void {
+
+
+    this.isLoggedIn = this._storageService.isLoggedOn();
+
+    if (this.isLoggedIn) {
+      const user = this._storageService.getSession();
+      
+    }
 
     this.ticketService.getAllTickets().subscribe({
       next: (response) => {
