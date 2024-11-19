326d91b666029931103c6a23bab36457b95c1510
bananites <codingtwitter@web.de>
2024-11-19 15:52:06 +0100

    env for homelab

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,6 @@
 export const environment = {
 
     production: false,
-    apiUrl: 'http://localhost:3000/api' // Development API
+    apiUrl: 'http://192.168.178.81:3000/api' // Development API HomeLab
+    // apiUrl: 'http://localhost:3000/api
 };
