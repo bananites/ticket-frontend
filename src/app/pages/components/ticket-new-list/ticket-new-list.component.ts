a88e6f46aa0aa1f0d0a29b2377cf0790376f5701
bananites <codingtwitter@web.de>
2025-01-25 21:14:32 +0100

    change path of files

--- src/app/pages/components/ticket-new-list/ticket-new-list.component.ts
+++ src/app/pages/components/ticket-new-list/ticket-new-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { Ticket } from '../../models/ticket';
+import { Ticket } from '../../../models/ticket';
 import { RouterModule } from '@angular/router';
-import { TicketService } from '../../services/ticket/ticket.service';
+import { TicketService } from '../../../services/ticket/ticket.service';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-ticket-new-list',
