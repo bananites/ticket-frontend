ed5cdb1e37bd754cc94680274e0f88cf1a4839e8
bananites <codingtwitter@web.de>
2025-01-25 21:12:41 +0100

    change api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@
 export const environment = {
 
     production: false,
-    apiUrl: 'http://192.168.178.81:3000/api' // Development API HomeLab
+    apiUrl: 'http://localhost:3000/api' // Development API HomeLab
     // apiUrl: 'http://localhost:3000/api
 };
