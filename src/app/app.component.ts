582456aa508f4ca4cb16e1e654c64c26ef5187d4
kraenkthetank <codingTwitter@web.de>
2025-02-08 14:09:42 +0100

    check for login and routing to login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { StorageService } from './services/storage/storage.service';
+import { EventBusService } from './shared/event-bus/event-bus.service';
+import { AuthUserService } from './services/auth/auth-user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +12,49 @@ import { StorageService } from './services/storage/storage.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
 
   isLoggedIn = false;
+  eventBusSub?: Subscription
 
   constructor(
     private storageService: StorageService,
     private _router: Router,
-  ){}
+    private _eventBusService: EventBusService,
+    private _authSerice: AuthUserService
+  ) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.storageService.isLoggedOn();
 
-    if(this.isLoggedIn){
+    if (this.isLoggedIn) {
       this._router.navigate(['']);
+    } else {
+
+      this._router.navigate(["login"]);
     }
 
-    this._router.navigate(["login"]);
-    
+
+    this.eventBusSub = this._eventBusService.on('logout', () => {
+      this.logout();
+    })
+
   }
   title = 'ticket-frontend';
+
+  logout() {
+    // TODO logout auth service
+    this._authSerice.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+
+        window.location.reload();
+
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
