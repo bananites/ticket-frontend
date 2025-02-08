dc8f6059ba1780869d44a6090f3ec699fc4dade9
kraenkthetank <codingTwitter@web.de>
2025-02-08 14:45:47 +0100

    set httpOptions const

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -3,10 +3,13 @@ import { Observable } from 'rxjs';
 import { User } from '../../models/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { SessionStorage, StorageService } from '../storage/storage.service';
 
-
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+  responseType: 'json' as const,
+};
 
 @Injectable({
   providedIn: 'root',
@@ -15,29 +18,14 @@ export class UserService {
 
   constructor(
     private http: HttpClient,
-    private storageService: StorageService,
   ) { }
 
   getAdminBoard(): Observable<any> {
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-      responseType: 'json' as const,
-    }
     return this.http.get<User>(environment.apiUrl + '/v1/users', httpOptions);
   }
 
   getAllUser(): Observable<User[]> {
-    const sessionStorage = this.storageService.getSession();
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer: '+ sessionStorage?.accessToken
-      }),
-      responseType: 'json' as const,
-    };
 
     return this.http.get<User[]>(environment.apiUrl + '/v1/users', httpOptions);
   }
